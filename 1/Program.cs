e56f2201065359650b451c8043e5ccf8e805bed6
«Dmitrii <bimas2006@mail.ru>
2022-06-07 11:40:20 +0300

    9 DZ

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+static int summ(int i, int a)
+{
+    if (i >= 10)
+    {
+        a = a + i % 10;
+        i = i / 10;
+        return summ(i, a);
+    }
+
+    else 
+        return a = a + i;
+}
+Console.WriteLine("Введите число:");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа {i} = {summ(i, 0)}");
---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+int sum(int M, int N)
+{
+    if (M == N)
+        return M;
+    if (M <= N)
+    {
+        M = M + sum(M+1, N);
+        return M;
+    }
+     else
+        N = N + sum(M, N+1);
+        return N;       
+};
+Console.WriteLine("Введите начальное число:");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число:");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Промежуточные данные:");
+Console.WriteLine($"Сумма цифр от {M} до {N} = {sum(M, N)}");
